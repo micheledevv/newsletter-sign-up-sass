2658c72328981bdedc977dcb59e55efdbcc43235
michele.pugliese <michele.puglese@lutech.it>
2024-07-09 16:55:55 +0200

    versione smartphone, tablet e desktop completa, aggiunta implementazione invio email + status formato email con regex

---
+++ javascripts/index.js
@@ -0,0 +1,34 @@
+document.getElementById("form").addEventListener("submit", function (e) {
+  const regForm =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  let inputElement = document.querySelector("input");
+  let statusEmail = document.querySelector("#box_input form #status_email p");
+  console.log(statusEmail);
+
+  let valueInput = inputElement.value;
+
+  e.preventDefault();
+
+  console.log(valueInput);
+
+  if (valueInput.match(regForm)) {
+    console.log("Email corretta");
+    inputElement.value = "";
+    statusEmail.innerHTML = "Email valida";
+    statusEmail.style.color = "green";
+
+    let objMail = {
+      email: inputElement.value,
+    };
+
+    emailjs
+      .send("service_tz44hoo","template_1fpv8qa", objMail)
+      .then((res) => console.log(res));
+  } else {
+    console.log("Email sbagliata");
+    inputElement.value = "";
+    statusEmail.innerHTML = "Email non valida";
+    statusEmail.style.color = "red";
+  }
+});
