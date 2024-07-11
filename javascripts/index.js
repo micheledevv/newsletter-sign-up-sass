ee99a269321d0326113f8cd7707dc481840c22df
michele.pugliese <michele.puglese@lutech.it>
2024-07-11 10:49:52 +0200

    creazione app cordova, ho buildato l'apk e funziona perfettamente, aggiunta immagine all'apk

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,9 @@
+(function () {
+  emailjs.init({
+      publicKey: "8MwpHkxQkI1iLptsP",
+  });
+})();
+
 document.getElementById("form").addEventListener("submit", function (e) {
   const regForm =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
